9f92511f0e75c5f2b3af11ee82816ce960960d93
melikesoygullucu <melikesoygul@hotmail.com>
2024-08-14 15:57:11 +0300

    Presenter updated.

--- CryptoCurrency-VIPER/Presenter.swift
+++ CryptoCurrency-VIPER/Presenter.swift
@@ -14,14 +14,24 @@ protocol AnyPresenter {
     var router : AnyRouter? {get set}
     var interactor : AnyInteractor? {get set}
     var view : AnyView? {get set}
+    
+    func interactorDidDownloadCrypto (result : Result<[Crypto], Error>)
 }
 
 class CryptoPresenter : AnyPresenter {
+
     var router: AnyRouter?
     
     var interactor: AnyInteractor?
     
     var view: AnyView?
     
-    
+    func interactorDidDownloadCrypto(result: Result<[Crypto], Error>) {
+        switch result {
+        case .success(let cryptos):
+            print("ok")
+        case .failure(let error):
+            print("error")
+        }
+    }
 }
